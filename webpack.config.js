a8d491ac408a6bb388622486572f5758739907b0
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-07-11 19:51:34 +0200

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from "node:path";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default {
+  context: path.resolve(__dirname, "src"),
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx"]
+  },
+  entry: {
+    app: "./index.tsx",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: "css-loader",
+          options: {
+            modules: {
+              exportLocalsConvention: "camelCase",
+              localIdentName: "[path][name]__[local]--[hash:base64:5]",
+            }
+          }
+        }, "sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      scriptLoading: "blocking"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css"
+    }),
+  ],
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8080,
+    devMiddleware: {
+      stats: "errors-only"
+    }
+  }
+};
