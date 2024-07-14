08889d9548cc71d615b7edd0d910ff6557cdbe98
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-07-14 19:40:06 +0200

    add linter, prettier and husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import * as classes from "./main.scss";
+import { createRoot } from 'react-dom/client';
+import * as classes from './main.scss';
 
-const root = createRoot(document.getElementById("root"));
-
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
 root.render(
   <>
     <h1 className={classes.title}>Hello React</h1>
